469ed3e5699fd5e5299904ca146d29b3471905c7
Tim Ermecke <tim.ermecke@web.de>
2018-05-15 10:01:45 +0200

    Implemented getBoardHTML() to create table based on  array content

--- Board.php
+++ Board.php
@@ -50,6 +50,48 @@ Class Board
 	{
 		return $this->board;
 	}
+	
+	/**
+	* @return string $output
+	* Returns current state of the board as whole HTML table
+	*/
+	public function getBoardHTML()
+	{
+		//initializing variables
+		$rowndex = '';
+		$output = '<table class="tic">';
+		
+		//iterating through rows
+		for($row, $row < 3, $row++)
+		{
+			$output .= '<tr>';
+			
+			//iterating through columns
+			for($col, $col < 3, $col++)
+			{
+				$output .= '<td>';
+				
+				//check if array position contains empty string
+				if($this->board[$row][$col] == '')
+				{
+					//add still free and clickable part to table output
+					$output .= '<input type="submit" class="reset field" name="cell-'.$row.'-'.$col.'" value="X" />';
+				}
+				//array position is already taken
+				else
+				{
+					//add array content to table output
+					$output .= '<span class="color'.$board[$row][$col].'">'.$board[$row][$col].'</span>';
+				}	
+				$output .= '</td>';
+			}	
+			$output .= '</tr>';
+		}
+		$output .= '</table>';
+		
+		//return completed HTML table of current board
+		return($output);
+	}
 }
 
 ?>
